8e35d8590cdca8f120f3428af6834a234568cc13
Filipe Cajado <filipecajado@ucl.br>
2024-02-18 16:36:22 -0300

    added list studies

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import Button from './components/Button';
+import Forms from './components/Forms'
+import List from './components/List';
 
 function App() {
   return (
     <div className="App">
-      <Button></Button>
+      <Forms/>
+      <List/>
     </div>
   );
 }
--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 class Button extends React.Component {
     render(){
+        const estaAtivo = true;
+        const color = 'red';
+        const styles ={
+            backgroundColor: estaAtivo ? 'blue' : 'red',
+            color: color
+            }
         return(
-            <button>
+            <button style={styles}>
                 Botão
             </button>
         )
